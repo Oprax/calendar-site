2c026057765cdabf5ae55d30a6aeb787fefa6cae
Oprax <oprax@me.com>
2016-01-11 07:00:06 +0100

    Remove coverall

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__.'/../bootstrap/autoload.php';
-
 passthru("./artisan --quiet migrate:reset");
 passthru("./artisan --quiet migrate");
+
+require __DIR__.'/../bootstrap/autoload.php';
